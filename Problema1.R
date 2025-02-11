4cd48b37b41900d382c217707fe8a631916ae9ff
joel.valls <joel.valls@auleseebe.upc.edu>
2025-02-11 09:15:45 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,36 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades <- mtcars
+mis_dades
+dim(mis_dades)
+
+
+mis_dades$qsec
+mis_dades$drat
+#promedio (centro de gravedad)
+mean(mis_dades$qsec)
+
+#mediana
+sort(mis_dades$drat)
+median(mis_dades$drat)
+
+#quartil
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$drat, 0.75)
+#interquantile Range (Q0,75-Q0,25)
+IQR(mis_dades$drat)
+
+#ILUSTRACIÓN MEDIANA Y QUARTILES
+boxplot(mis_dades$drat)
+quantile(mis_dades$mpg, 0.18)
+
+IQR(mis_dades$cyl)
+
+#varianza
+var(mis_dades$qsec)
+
+#Desviación típica
+sd(mis_dades$cyl)
+
+
+
